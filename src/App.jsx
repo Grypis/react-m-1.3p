8dcf88fed69b46ca0deacd98ce6a74ba4b0196b6
grypis <grypisnecros@gmail.com>
2026-01-11 17:54:26 +0200

    4 task

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ import {
 } from "components";
 import article from "data/article.json";
 import stats from "data/stats.json";
-/* import stats from "data/stats.json";
 import transactions from "data/transactions.json";
-import forbes from "./data/forbes.json"; */
+// import stats from "data/stats.json";
+// import forbes from "./data/forbes.json";
 
 export const App = () => {
   return (
@@ -33,10 +33,10 @@ export const App = () => {
         <Statistics stats={stats} />
 
         {/* <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList /> */}
 
         <Heading title="Task 4 Crypto history" top bottom />
-        <CryptoHistory /> */}
+        <CryptoHistory items={transactions} />
       </Container>
     </Section>
   );
--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,7 @@
+// import { formatDate } from "helpers/formatDate";
 import { formatDate } from "helpers/formatDate";
 import style from "./CryptoHistory.module.css";
+
 const tableHeaders = ["№", "price", "amount", "date"];
 
 export const CryptoHistory = ({ items }) => {
@@ -7,20 +9,20 @@ export const CryptoHistory = ({ items }) => {
     <table className={style.table}>
       <thead className={style.thead}>
         <tr>
-          {tableHeaders.map((el, index) => (
+          {tableHeaders.map((item, index) => (
             <th key={index} className={style.th}>
-              {el}
+              {item}
             </th>
           ))}
         </tr>
       </thead>
 
       <tbody>
-        {items.map(({ id, amount, price, date }, index) => (
+        {items.map(({ id, amount, date, price }, index) => (
           <tr key={id} className={style.tr}>
             <td className={style.td}>{index + 1}</td>
-            <td className={style.td}>{amount}</td>
             <td className={style.td}>{price}</td>
+            <td className={style.td}>{amount}</td>
             <td className={style.td}>{formatDate(date)}</td>
           </tr>
         ))}
